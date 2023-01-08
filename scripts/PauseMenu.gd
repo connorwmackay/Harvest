a8c77aa251afce3b6418c1fe402e71eed7da0b04
Connor Mackay <44244596+connorwmackay@users.noreply.github.com>
2023-01-08 16:35:13 +0000

    Pause Menu

---
+++ scripts/PauseMenu.gd
@@ -0,0 +1,23 @@
+extends PanelContainer
+
+onready var selection_mode = get_tree().get_current_scene().get_node("SelectedTile")
+
+func _process(delta):
+	if Input.is_action_just_pressed("toggle_pause"):
+		visible = !visible
+
+func _on_ReturnButton_pressed():
+	visible = !visible
+
+func _on_MainMenuButton_pressed():
+	get_tree().change_scene("res://MainMenu.tscn")
+
+func _on_QuitButton_pressed():
+	get_tree().quit()
+
+func _on_PauseMenu_mouse_entered():
+	selection_mode.using_ui = true
+
+func _on_PauseMenu_mouse_exited():
+	if not get_global_rect().has_point(self.get_global_mouse_position()):
+		selection_mode.using_ui = false
